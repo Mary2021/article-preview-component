44282bee2bb3fbbadfd0452dcf2f7e78e51e905b
Mary <mary.pommer@tptlive.ee>
2024-11-11 19:05:14 +0200

    add desktop layout

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ Users should be able to:
 
 ### Screenshot
 
-![](./desktop-preview.jpg)
+![](./design/desktop-preview.jpg)
 
 ### Links
 
--- main.js
+++ main.js
@@ -1,6 +1,26 @@
 function hide() {
-    const container = document.getElementById('authorContainer')
-    container.classList.add("hidden")  
-    const shareContainer = document.getElementById('shareContainer')
-    shareContainer.classList.remove("hidden")
+    
+    if (screen.width > 768){
+        
+        // const container = document.getElementById('authorContainer')
+        
+        // container.classList.add("hidden")  
+        // console.log(container.classList)
+        const desktopShareContainer = document.getElementById('desktopShareContainer')
+        console.log(desktopShareContainer)
+        // desktopShareContainer.classList.remove("hidden")
+        desktopShareContainer.style.display = 'grid'
+        const triangleDown = document.getElementById('triangleDown')
+        console.log(triangleDown)
+        console.log(triangleDown.classList)
+        triangleDown.classList.remove("hidden") 
+        console.log(triangleDown.classList)
+        
+    } 
+    else {
+        const container = document.getElementById('authorContainer')
+        container.classList.add("hidden")  
+        const shareContainer = document.getElementById('shareContainer')
+        shareContainer.classList.remove("hidden")
+    }
 }
